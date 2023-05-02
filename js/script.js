b9abca686102844dd47677f7831fec5d1255f09c
slammajamma28 <slammajamma28@gmail.com>
2023-05-02 14:47:21 -0400

    Updating tracker via automation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
   // Update the css percentages for progress bar
   //background: -webkit-linear-gradient(left, blue 72%, white 72%);
   //background:    -moz-linear-gradient(left, blue 72%, white 72%);
@@ -16,8 +16,24 @@ document.addEventListener("DOMContentLoaded", function () {
     background:      -o-linear-gradient(left, blue 72%, white 72%);
     background:         linear-gradient(to right, blue 72%, white 72%);
   `
-  document.getElementsByClassName("percentageFill").style.cssText = progressBarStyle;
-}, false);
+  var rows = document.getElementsByClassName("percentageFill");
+  for (row in rows) {
+    row.style = progressBarStyle;
+  }
+}, false);*/
+
+$(document).ready(function () {
+  console.log("Jquery is a go!");
+  const currentVal = document.getElementById("currentCount").innerText;
+  $(".percentageFill").css(
+      {"color": "black",
+      "border": "1px solid black",
+      "background": "-webkit-linear-gradient(left, blue ${currentVal}%, white ${currentVal}%)",
+      "background":    "-moz-linear-gradient(left, blue ${currentVal}%, white ${currentVal}%)",
+      "background":     "-ms-linear-gradient(left, blue ${currentVal}%, white ${currentVal}%)",
+      "background":      "-o-linear-gradient(left, blue ${currentVal}%, white ${currentVal}%)",
+      "background":         "linear-gradient(to right, blue ${currentVal}%, white ${currentVal}%"})
+});
 
 function openTab(evt, tabName) {
     // Declare all variables
